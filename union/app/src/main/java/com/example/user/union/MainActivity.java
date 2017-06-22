fcf104030bb37c77ef688222029707ead546a119
efsolano1 <efsolano1@gmail.com>
2017-06-22 09:37:51 -0500

    avanceAndroid

--- union/app/src/main/java/com/example/user/union/MainActivity.java
+++ union/app/src/main/java/com/example/user/union/MainActivity.java
@@ -1,13 +1,58 @@
 package com.example.user.union;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn_nuevo;
+    Button btn_2;
+    Button btn_sinc;
+    Button btn_cam;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_nuevo=(Button) findViewById(R.id.bt1);
+        btn_nuevo.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent
+                        (MainActivity.this, Nuevo.class);
+                startActivity(intent);
+            }
+            });
+
+        btn_2=(Button) findViewById(R.id.bt3);
+        btn_2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent1 = new Intent
+                        (MainActivity.this, lista.class);
+                startActivity(intent1);
+            }
+        });
+
+        btn_sinc=(Button) findViewById(R.id.bt2);
+        btn_sinc.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent1 = new Intent
+                        (MainActivity.this, Sincronizar.class);
+                startActivity(intent1);
+            }
+        });
+        btn_cam=(Button) findViewById(R.id.bt4);
+        btn_cam.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent1 = new Intent
+                        (MainActivity.this, Camara.class);
+                startActivity(intent1);
+            }
+    });
     }
 }
---
+++ union/app/src/main/java/com/example/user/union/lista.java
@@ -0,0 +1,42 @@
+package com.example.user.union;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+public class lista extends AppCompatActivity {
+    Button btn_nuevo;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        btn_nuevo=(Button) findViewById(R.id.bt3);
+        btn_nuevo.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent
+                        (lista.this, MainActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+    }
+
+}
